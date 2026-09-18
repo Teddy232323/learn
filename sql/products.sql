2dc4ad82b717aa1ad3d7ee99c6478c22d2894e4a
Teddy <teddy@example.com>
2026-09-18 09:50:25 +0800

    练习SQL基础教程2版习题第2-3章

--- sql/products.sql
+++ sql/products.sql
@@ -11,3 +11,36 @@ values
 ('香蕉',80);
 
 select * from products;
+
+--26-09-17，练习题>SQL基础教程2版
+--2.1
+select product_name,regist_date
+	from product
+	where regist_date > '2009-04-28';
+--2.2
+select * from product where purchase_price is null;
+select * from product where purchase_price is not null;
+--2.3
+select product_name,sale_price,purchase_price
+	from product 
+	where sale_price >= purchase_price+500;
+
+--3.1
+select product_type,sum(sale_price )
+	from product p
+	where regist_date > '2009-09-01'
+	group by p.product_type;
+--3.2
+select product_type,sum(sale_price) as sum1,sum(purchase_price ) as sum2
+	from product
+	group by product_type 
+	having sum(sale_price ) >=1.5*sum(purchase_price );
+--3.3
+select * from product p 
+	order by regist_date desc,sale_price;
+select * from product;
+
+
+
+
+
