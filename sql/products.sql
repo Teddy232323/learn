1239c6e7bfda0b292c9422fff16089f79c4298f4
Teddy <teddy@example.com>
2026-09-20 10:33:46 +0800

    SQL基础教程练习26-9-20

--- sql/products.sql
+++ sql/products.sql
@@ -28,8 +28,6 @@ select * from addressbook3;
 --1.3
 drop table addressbook3;
 alter table addressbook rename to addressbook3;
-
-
 --2.1
 select product_name,regist_date
 	from product
@@ -65,9 +63,42 @@ select product_type,sum(sale_price) as sum1,sum(purchase_price ) as sum2
 select * from product p 
 	order by regist_date desc,sale_price;
 select * from product;
-
-
-
+--4.1
+begin transaction;
+	insert into product values
+	('0009','T恤衫','衣服',1000,500,'2008-09-20'),
+	('0010','打孔器','办公用品',500,320,'2008-09-11'),
+	('0011','运动T恤','衣服',4000,2800,null);
+rollback;
+select * from product;
+--4.2
+insert into product 
+	select * from product;
+alter table product drop constraint product_pkey;
+--4.3
+select  * from productmargin;
+create table productmargin1(
+product_id char(4) not null,
+product_name varchar(100) not null,
+sale_price integer,
+purchase_price integer,
+maigin integer,
+primary key(product_id));
+insert into productmargin1(product_id,product_name,sale_price,purchase_price,margin)
+values 
+('0001','T恤衫',1000,500,500),
+('0002','打孔器',500,320,180),
+('0003','运动T恤',4000,2800,1200);
+alter table productmargin1 rename column maigin to margin; 
+select * from productmargin;
+--4.4
+update  productmargin1
+set sale_price=3000
+where product_id='0003';
+select * from productmargin1;
+update productmargin1
+set margin=sale_price-purchase_price;
+--5.1
 
 
 select * from products;--模块1：演示完整提交流程
